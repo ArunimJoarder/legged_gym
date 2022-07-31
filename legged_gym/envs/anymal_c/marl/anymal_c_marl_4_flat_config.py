ffbb3a7b073f877e63f90e482f32961aff248e6b
Arunim Joarder <reevu123@gmail.com>
2022-07-31 19:43:24 +0530

    working MARL - common observations

--- legged_gym/envs/anymal_c/marl/anymal_c_marl_4_flat_config.py
+++ legged_gym/envs/anymal_c/marl/anymal_c_marl_4_flat_config.py
@@ -30,52 +30,86 @@
 
 from legged_gym.envs import AnymalCFlatCfg, AnymalCFlatCfgPPO
 
-class AnymalCMARLCfg( AnymalCFlatCfg ):
+class AnymalCMARL_4_FlatCfg( AnymalCFlatCfg ):
 	class env( AnymalCFlatCfg.env ):
 		num_agents = 4
+		num_dof_per_agent = 12//num_agents
+		num_actions_per_agent = 12//num_agents
+		num_obs_per_agent = 48
+		num_privileged_obs_per_agent = None
+
+	class asset( AnymalCFlatCfg.asset ):
+		# penalize_contacts_on = ["LF_SHANK", "LF_THIGH", "LH_SHANK", "LH_THIGH", "RF_SHANK", "RF_THIGH", "RH_SHANK", "RH_THIGH"]
+		pass
 
 	class rewards( AnymalCFlatCfg.rewards ):
 		class scales( AnymalCFlatCfg.rewards.scales ):
+			pass
+			# termination = -0.0
+			# tracking_lin_vel = 1.0
+			# tracking_ang_vel = 0.5
+			# lin_vel_z = -2.0
+			# ang_vel_xy = -0.05
+			# dof_vel = -0.
+			# dof_acc = -2.5e-7
+			# base_height = -0. 
+			# collision = -1.
+			# feet_stumble = -0.0 
+			# action_rate = -0.01
+			# stand_still = -0.
+			# orientation = -5.0
+			# torques = -0.000025
+			# feet_air_time = 2.
+			
+			dof_acc = 0.
+			dof_vel = 0.
+			feet_air_time =  2.
+			action_rate = 0.
+			torques = 0.
+			
+			orientation = -5.0
+		
 			termination = -0.0
 			tracking_lin_vel = 1.0
 			tracking_ang_vel = 0.5
 			lin_vel_z = -2.0
 			ang_vel_xy = -0.05
-			orientation = -5.0
-			dof_vel = 0.
-			dof_acc = 0.
-			base_height = -0. 
-			feet_air_time =  0.
+			base_height = 0. 
 			collision = 0.
-			feet_stumble = 0.
-			action_rate = 0.
-			stand_still = 0.
-			
-			leg_1_torques = -0.000025
-			leg_1_dof_acc = -2.5e-7
-			leg_1_feet_air_time = 2.
-			leg_1_collision = -1.
-			leg_1_action_rate = -0.01
+			feet_stumble = -0.0 
+			stand_still = -0.
 
-			leg_2_torques = -0.000025
-			leg_2_dof_acc = -2.5e-7
-			leg_2_feet_air_time = 2.
-			leg_2_collision = -1.
-			leg_2_action_rate = -0.01
+			agent_1_torques = -0.000025
+			agent_1_dof_acc = -2.5e-7
+			agent_1_action_rate = -0.01
+			# agent_1_foot_air_time =  2.
+			agent_1_collision = -1.
 
-			leg_3_torques = -0.000025
-			leg_3_dof_acc = -2.5e-7
-			leg_3_feet_air_time = 2.
-			leg_3_collision = -1.
-			leg_3_action_rate = -0.01
+			agent_2_torques = -0.000025
+			agent_2_dof_acc = -2.5e-7
+			agent_2_action_rate = -0.01
+			# agent_2_foot_air_time =  2.
+			agent_2_collision = -1.
 
-			leg_4_torques = -0.000025
-			leg_4_dof_acc = -2.5e-7
-			leg_4_feet_air_time = 2.
-			leg_4_collision = -1.
-			leg_4_action_rate = -0.01
+			agent_3_torques = -0.000025
+			agent_3_dof_acc = -2.5e-7
+			agent_3_action_rate = -0.01
+			# agent_3_foot_air_time =  2.
+			agent_3_collision = -1.
+
+			agent_4_torques = -0.000025
+			agent_4_dof_acc = -2.5e-7
+			agent_4_action_rate = -0.01
+			# agent_4_foot_air_time =  2.
+			agent_4_collision = -1.
 
 
-class AnymalCMARLCfgPPO( AnymalCFlatCfgPPO ):
-    class runner ( AnymalCFlatCfgPPO.runner):
-        experiment_name = 'marl_anymal_c'
+class AnymalCMARL_4_FlatCfgPPO( AnymalCFlatCfgPPO ):
+	class policy( AnymalCFlatCfgPPO.policy ):
+		actor_hidden_dims = [128, 32, 16]
+		critic_hidden_dims = [128, 32, 16]
+		activation = 'elu' # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+		pass
+
+	class runner ( AnymalCFlatCfgPPO.runner):
+		experiment_name = 'marl_4_flat_anymal_c'
